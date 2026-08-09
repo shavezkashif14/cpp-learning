aafadec1006b3aa5d01ba3d51652424a0d60ab63
Shavez Kashif <shavezkashif10@gmail.com>
2026-08-09 16:35:22 +0500

    Add Day 1 and Day 2 C++ exercises

---
+++ 02_variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int age = 17;
+    double percentage = 87.5;
+    char grade = 'A';
+    bool passed = true;
+    std::string name = "Shavez";
+
+    std::cout << "Age: " << age << '\n';
+    std::cout << "Percentage: " << percentage << '\n';
+    std::cout << "Grade: " << grade << '\n';
+    std::cout << "Passed: " << passed << '\n';
+    std::cout << "Name: " << name << '\n';
+
+    return 0;
+}
